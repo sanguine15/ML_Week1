b97985341017424c0009af6483b61e22a4d8da89
sanguine15 <bhawanasony15@gmail.com>
2025-05-01 16:16:37 +0530

    Add files via upload

---
+++ task2.py
@@ -0,0 +1,12 @@
+# Simple Grocery Bill Calculator
+
+price = float(input("Enter item price: ₹"))
+quantity = int(input("Enter item quantity: "))
+
+total = price * quantity
+gst = total * 0.18
+final_amount = total + gst
+
+print(f"\nTotal: ₹{total:.2f}")
+print(f"GST (18%): ₹{gst:.2f}")
+print(f"Final Bill: ₹{final_amount:.2f}")
