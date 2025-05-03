17feb4d3614096e245abc5310056d2f3c88c30bd
sanguine15 <bhawanasony15@gmail.com>
2025-05-03 19:56:41 +0530

    Add files via upload

---
+++ task02.py
@@ -0,0 +1,10 @@
+# Task 2
+students = [
+    (101, "Alice", 20),
+    (102, "Bob", 21),
+    (103, "Charlie", 19)
+]
+
+print("\nStudent Information:")
+for roll, name, age in students:
+    print(f"Roll No: {roll}, Name: {name}, Age: {age}")
